077f0d7da5a49cbeae8856ef2e4c4f8c7634a1ef
Julien Moreau <jmoreau@veepee.com>
2014-02-26 14:50:01 +0100

    Improve cvs-edit.sh & add exec perm on all scripts

--- cvs-edit.sh
+++ cvs-edit.sh
@@ -2,11 +2,11 @@
 
 # cve <file1> [<file2> ...]
 #
-# Pour éditer un ou plusieurs fichier(s) existant(s) sous CVS.
+# Pour Ã©diter un ou plusieurs fichier(s) existant(s) sous CVS.
 # - fait un "cvs update" si besoin (status "Needs patch")
-# - fait un "cvs edit" pour déclarer aux autres users qu'on édite le fichier
+# - fait un "cvs edit" pour dÃ©clarer aux autres users qu'on Ã©dite le fichier
 #	(voir la commande "cvs editors")
-# - utilise l'éditeur préféré déclaré dans la variable $EDITOR
+# - utilise l'Ã©diteur prÃ©fÃ©rÃ© dÃ©clarÃ© dans la variable $EDITOR
 
 # (c) 2003-2005 Julien Moreau
 # 2002-03 Creation
@@ -16,29 +16,30 @@
 # Last version under CVS (GMT):
 # $Header: /cvs/CVS-tools/cve.sh,v 1.6 2012-11-23 10:08:38 jmoreau Exp $
 
-nbps=1				# Nombre de paramètres souhaités (sans option)
+nbps=1				# Nombre de paramÃ¨tres souhaitÃ©s (sans option)
 nom_cmde=`basename $0`		# Nom de la commande
 usage="Usage: $nom_cmde [-h]"	# Message d'aide
 usage=$usage"\n\tAffiche ce message d'aide.\n"
 usage=$usage"\nUsage: $nom_cmde <filename> [...]"
-usage=$usage"\n\tÉdite des fichiers CVS en posant des verrous d'écriture."
+usage=$usage"\n\tÃ‰dite des fichiers CVS en posant des verrous d'Ã©criture."
 
-if test `uname` != "HP-UX" ; then e="-e" ; fi	# Compatibilité HP-UX/Linux
+if test `uname` != "HP-UX" ; then e="-e" ; fi	# CompatibilitÃ© HP-UX/Linux
 
-if [ $# -lt 0 -o "$1" = "-h" -o "$1" = "--help" ] ; then # Vérif params
+if [ $# -lt 0 -o "$1" = "-h" -o "$1" = "--help" ] ; then # VÃ©rif params
 	echo $e $usage 1>&2 ; exit 2			 # Aide puis fin
 fi
 
-if test -z "$EDITOR" ; then
-  if test -z "$USER" ; then export USER=`whoami` ; fi
+if test -z "$EDITOR"
+then
+  if test -z "$USER"
+  then export USER=`whoami`
+  fi
+
   echo "$USER"|grep -q moreau
-  if [ "$?" -eq 0 ] ; then
-    export EDITOR='gvim -geom 128x50'
-  else
-    if test "$USER" = "gkieffer"
-    then export EDITOR=emacs
-    else export EDITOR=vi
-    fi
+
+  if [ -n "$DISPLAY" -a "$?" -eq 0 ]
+  then export EDITOR='gvim -geom 128x50'
+  else export EDITOR=vi
   fi
 fi
 
@@ -67,11 +68,11 @@ do
     ki=""
     if [ "$nb_editors" -eq 1 ] ; then ki=`cvs editors "$file"|cut -f2` ; fi
     if test "$ki" = `whoami` ; then
-      echo "Vous êtes déjà éditeur de ce fichier."
+      echo "Vous Ãªtes dÃ©jÃ  Ã©diteur de ce fichier."
       sleep 2
       $EDITOR "$file"
     else
-      echo -e "Il y a déjà $nb_editors éditeur(s) de $file !\n$editors"
+      echo -e "Il y a dÃ©jÃ  $nb_editors Ã©diteur(s) de $file !\n$editors"
     fi
   fi
 
