e66bd23aa2cdddaa1fd81db6a67704b19bf1dd4a
Julien Moreau <jmoreau@veepee.com>
2014-05-09 14:30:44 +0200

    Add some comments to phpcs-wrapper.sh

--- phpcs-wrapper.sh
+++ phpcs-wrapper.sh
@@ -4,12 +4,15 @@
 
 files=$*
 
+# Check PHP syntax for errors:
 for f in $files
 do php -n -l -f "$f" || exit $?
 done |grep -v ^'No syntax errors detected in '
 
-# phpcs $files |grep -ve ^$ -e '---'  # returns 1 even if all right :-/
-
+# Use PhpCodeSniffer to standardize the PHP syntax:
+#   First way:
+#phpcs $files |grep -ve ^$ -e '---'  # returns 1 even if all right :-/
+#   Second way:
 output="/tmp/phpcs.out"
 phpcs $files > "$output" 2>&1
 ret=$?
